ab74fc1c1575b538a0f11ae15b4f184e2c18fe6f
Kate Stanley <katheris@uk.ibm.com>
2015-07-03 17:14:08 +0100

    Add initial jaxrs code to call into database.

--- 12-factor-application/src/main/java/net/wasdev/twelvefactorapp/JaxrsHttpReceiver.java
+++ 12-factor-application/src/main/java/net/wasdev/twelvefactorapp/JaxrsHttpReceiver.java
@@ -3,17 +3,36 @@ package net.wasdev.twelvefactorapp;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/")
 public class JaxrsHttpReceiver {
+	String databaseName = "testdb";
 	
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getResponse() {
-		String response = "Hello this is a response";
-		return Response.ok(response).build();
+		String response = "Hello this is a response ";
+		String dbFiles = getDatabaseFiles();
+		return Response.ok(response + dbFiles).build();
+	}
+	
+	public String getDatabaseFiles() {
+		Client client = ClientBuilder.newClient();
+		String dbUrl = System.getenv("dbUrl");
+		String url = dbUrl;
+		WebTarget target = client.target(url);
+		Invocation.Builder invoBuild  = target.request(MediaType.APPLICATION_JSON);
+		Response httpResponse = invoBuild.get();
+		String contents = httpResponse.readEntity(String.class);
+		httpResponse.close();
+		return contents + " url was " + url;
+		
 	}
 
 }
