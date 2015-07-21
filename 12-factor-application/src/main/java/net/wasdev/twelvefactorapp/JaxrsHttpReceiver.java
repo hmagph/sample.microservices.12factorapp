25872d6015723ae8c87ea7fb9b569bc5275b532a
Kate Stanley <katheris@uk.ibm.com>
2015-07-21 13:02:28 +0100

    Add in a POST request so we can store data in the database.

--- 12-factor-application/src/main/java/net/wasdev/twelvefactorapp/JaxrsHttpReceiver.java
+++ 12-factor-application/src/main/java/net/wasdev/twelvefactorapp/JaxrsHttpReceiver.java
@@ -10,11 +10,13 @@ import javax.json.JsonObject;
 import javax.json.JsonReader;
 import javax.json.JsonString;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
@@ -22,13 +24,13 @@ import javax.ws.rs.core.Response;
 
 @Path("/")
 public class JaxrsHttpReceiver {
-	String databaseName = "testdb";
+	String databaseName = "items";
 	
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getResponse() throws NullPointerException, IOException {
 		String dbFiles = getDatabaseFiles("/_all_dbs");
-		String items = getDatabaseFiles("/items/_all_docs");
+		String items = getDatabaseFiles("/" + databaseName + "/_all_docs");
 		return Response.ok("All databases: " + dbFiles + "Items database: " + items).build();
 	}
 	
@@ -36,6 +38,16 @@ public class JaxrsHttpReceiver {
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response putResponse(String databaseName) throws NullPointerException, IOException {
 		String contents = createDatabase(databaseName);
+		return Response.ok("Response is " + contents).build();
+	}
+	
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response postResponse(String data) throws NullPointerException, IOException {
+		InputStream is = new ByteArrayInputStream(data.getBytes());
+		JsonReader reader = Json.createReader(is);
+		JsonObject jsonData = reader.readObject();
+		String contents = storeData(jsonData);
 		return Response.ok("Response is " + contents).build();
 	}
 	
@@ -64,13 +76,13 @@ public class JaxrsHttpReceiver {
 	}
 	
 	public String createDatabase(String name) throws NullPointerException, IOException {
-		System.out.println("Creating database called " + databaseName);
+		System.out.println("Creating database called " + name);
 
 		String[] credentials = getCredentials();
 		String username = credentials[0];
 		String password = credentials[1];
 		String url = credentials[2];
-		String fullUrl = url + "/" + databaseName;
+		String fullUrl = url + "/" + name;
 		System.out.println("Found url " + fullUrl);		
 		String usernameAndPassword = username + ":" + password;
 				
@@ -81,6 +93,30 @@ public class JaxrsHttpReceiver {
 		WebTarget target = client.target(fullUrl);
 		Invocation.Builder invoBuild  = target.request(MediaType.APPLICATION_JSON).header(authorizationHeaderName, authorizationHeaderValue);
 		Response httpResponse = invoBuild.buildPut(null).invoke();
+		String contents = httpResponse.readEntity(String.class);
+		httpResponse.close();
+		return contents;
+	}
+	
+	public String storeData(JsonObject data) throws NullPointerException, IOException {
+		System.out.println("Storing data " + data);
+
+		String[] credentials = getCredentials();
+		String username = credentials[0];
+		String password = credentials[1];
+		String url = credentials[2];
+		String fullUrl = url + "/" + databaseName + "/";
+		System.out.println("Found url " + fullUrl);		
+		String usernameAndPassword = username + ":" + password;
+				
+		String authorizationHeaderName = "Authorization";
+		String authorizationHeaderValue = "Basic " + javax.xml.bind.DatatypeConverter.printBase64Binary(usernameAndPassword.getBytes());		
+			
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target(fullUrl);
+		Invocation.Builder invoBuild  = target.request(MediaType.APPLICATION_JSON).header(authorizationHeaderName, authorizationHeaderValue);
+		Entity<myObject> ent = Entity.entity(new myObject(data), MediaType.APPLICATION_JSON);
+		Response httpResponse = invoBuild.buildPost(ent).invoke();
 		String contents = httpResponse.readEntity(String.class);
 		httpResponse.close();
 		return contents;
---
+++ 12-factor-application/src/main/java/net/wasdev/twelvefactorapp/myObject.java
@@ -0,0 +1,17 @@
+package net.wasdev.twelvefactorapp;
+
+import javax.json.JsonObject;
+
+public class myObject {
+
+	private JsonObject myData;
+	
+	public myObject(JsonObject data) {
+		this.myData = data;
+	}
+	
+	public JsonObject getData() {
+		return this.myData;
+	}
+	
+}
